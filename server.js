7ff98da52953e829ea4fa97543a94dfa80f32a9a
Jialu Deng <jialudeng@Jialus-MacBook-Pro.local>
2020-02-05 16:18:30 -0800

    added all four services as submodules

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const axios = require('axios')
+
+const app = express()
+const port = 3000
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+app.get('/seed', (req, res) => {
+  axios.get('http://localhost:3003/seed')
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+});
+
+app.get('/houses', (req, res) => {
+  axios.get('http://localhost:3004/houses')
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+});
+
+app.get('/listings', (req, res) => {
+  axios.get('http://localhost:3004/listings')
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+});
+
+
+
+app.get('/api/data/:id', (req, res) =>{
+  const { id } = req.params;
+  axios.get(`http://localhost:3002/api/data/${id}`)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+});
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
