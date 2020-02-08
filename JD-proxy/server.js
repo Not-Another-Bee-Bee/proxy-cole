6af16dc17bfb1797180fd521e9669e3cb6de1297
Jialu Deng <jialudeng@Jialus-MacBook-Pro.local>
2020-02-08 15:25:41 -0800

    added public ip addresses

--- JD-proxy/server.js
+++ JD-proxy/server.js
@@ -21,35 +21,35 @@ app.get('/seed', (req, res) => {
     })
 });
 
-// app.get('/houses', (req, res) => {
-//   axios.get('http://localhost:3004/houses')
-//     .then(response => {
-//       res.send(response.data);
-//     })
-//     .catch(err => {
-//       console.log(err)
-//     })
-// });
+app.get('/houses', (req, res) => {
+  axios.get('http://18.144.88.205:3004/houses')
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+});
 
-// app.get('/listings', (req, res) => {
-//   axios.get('http://localhost:3004/listings')
-//     .then(response => {
-//       res.send(response.data);
-//     })
-//     .catch(err => {
-//       console.log(err)
-//     })
-// });
+app.get('/listings', (req, res) => {
+  axios.get('http://52.53.150.79:3001/listings')
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+});
 
-// app.get('/api/summary/data/:id', (req, res) => {
-//   const id = req.params.id;
-//   axios.get(`http://localhost:3002/api/summary/data/${id}`)
-//   .then((response) => {
-//     res.send(response.data);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   })
-// });
+app.get('/api/summary/data/:id', (req, res) => {
+  const id = req.params.id;
+  axios.get(`http://3.17.76.247:3002/api/summary/data/${id}`)
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
