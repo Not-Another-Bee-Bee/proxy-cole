b22fe7d508117b0270325065f0fe87bdb9cd9c44
Jialu Deng <jialudeng@Jialus-MacBook-Pro.local>
2020-02-11 16:16:08 -0800

    fixed all path for localhosting

--- JD-proxy/server.js
+++ JD-proxy/server.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 
 
 app.get('/seed', (req, res) => {
-  axios.get('http://18.223.108.8:3003/seed')
+  axios.get('http://localhost:3003/seed')
     .then(response => {
       res.send(response.data);
     })
@@ -22,7 +22,7 @@ app.get('/seed', (req, res) => {
 });
 
 app.get('/houses', (req, res) => {
-  axios.get('http://18.144.88.205:3004/houses')
+  axios.get('http://localhost:3004/houses')
     .then(response => {
       res.send(response.data);
     })
@@ -32,7 +32,7 @@ app.get('/houses', (req, res) => {
 });
 
 app.get('/listings', (req, res) => {
-  axios.get('http://52.53.150.79:3001/listings')
+  axios.get('http://localhost:3001/listings')
     .then(response => {
       res.send(response.data);
     })
@@ -43,7 +43,7 @@ app.get('/listings', (req, res) => {
 
 app.get('/api/summary/data/:id', (req, res) => {
   const id = req.params.id;
-  axios.get(`http://3.17.76.247:3002/api/summary/data/${id}`)
+  axios.get(`http://localhost:3002/api/summary/data/${id}`)
   .then((response) => {
     res.send(response.data);
   })
--- JD-proxy/startserver.sh
+++ JD-proxy/startserver.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 
-cd ../JD-service
-npm run start &
-npm run webpack & 
+cd ../AK-service
+npm run build &
+npm run seedDB &
+npm run server &
 
-cd ../HT-service
+cd ../HT-service 
+npm run seedDB &
+npm run start &
+
+cd ../JD-service
 npm run start & 
 
 cd ../sb-service
 npm run seed &
-
-cd ../AK-service
-npm run seedDB &
-npm run server &
 
 cd ../JD-proxy
 npm run start &
