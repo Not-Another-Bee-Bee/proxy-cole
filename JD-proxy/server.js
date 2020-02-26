797b132696fe59dbcd47d39db8e86889c6283a0b
ColeHuntley83 <calrugby1@gmail.com>
2020-02-25 20:08:09 -0800

    done

--- JD-proxy/server.js
+++ JD-proxy/server.js
@@ -28,7 +28,7 @@ app.get('/listings/:listingID', (req, res) => {
 
 
 app.get('/bundle.js', (req, res) => {
-  axios.get(`http://13.57.25.65:3001:3000/bundle.js`)
+  axios.get(`http://13.57.25.65:3001/bundle.js`)
   .then(response => {
   
     res.send(response.data);
